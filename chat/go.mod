9aa4e21cb6f7315dba7bed6a8a420fe3f7fb90ac
anraku <anraku90@gmail.com>
2018-11-13 07:31:11 +0900

    [add] add web framework 'echo'

--- chat/go.mod
+++ chat/go.mod
@@ -6,5 +6,12 @@ require (
 	github.com/gorilla/websocket v1.4.0
 	github.com/jinzhu/gorm v1.9.1
 	github.com/jinzhu/inflection v0.0.0-20180308033659-04140366298a // indirect
+	github.com/labstack/echo v3.3.5+incompatible
+	github.com/labstack/gommon v0.2.8 // indirect
+	github.com/mattn/go-colorable v0.0.9 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v0.0.0-20170224212429-dcecefd839c4 // indirect
+	golang.org/x/crypto v0.0.0-20181112202954-3d3f9f413869 // indirect
 	google.golang.org/appengine v1.3.0
 )
--- chat/main.go
+++ chat/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -13,18 +14,36 @@ import (
 	"github.com/anraku/chat/repository"
 	"github.com/anraku/chat/trace"
 	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
 	"google.golang.org/appengine"
 )
 
 // templは1つのテンプレートを表します
+type Template struct {
+	// once     sync.Once
+	// filename string
+	templates *template.Template
+}
+
+// /chatで使ってる
 type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
 }
 
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+var roomArray = make([]room, 1000, 2000)
+
 // ServeHTTPはHTTPリクエストを処理します
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		panic(err)
+	}
 	t.once.Do(func() {
 		t.templ =
 			template.Must(template.ParseFiles(filepath.Join("templates",
@@ -38,6 +57,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		uri = "ws://" + r.Host
 	}
 	data := map[string]interface{}{
+		"ID":  r.Form.Get("room_id"),
 		"Uri": uri,
 	}
 	if err := t.templ.Execute(w, data); err != nil {
@@ -45,47 +65,7 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var DB *gorm.DB
-
-func main() {
-	db, err := database.Connect()
-	if err != nil {
-		panic(err)
-	}
-	DB = db
-	defer db.Close()
-
-	r := newRoom()
-	r.tracer = trace.New(os.Stdout)
-	// static file path
-	http.HandleFunc("/vendor/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})
-	http.HandleFunc("/css/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})
-	http.HandleFunc("/avatars/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, r.URL.Path[1:])
-	})
-
-	http.HandleFunc("/index", Index)
-	http.Handle("/chat", &templateHandler{filename: "chat.html"})
-	http.Handle("/room", r)
-	http.HandleFunc("/room/new", NewRoom)
-	http.HandleFunc("/room/create", CreateRoom)
-	// チャットルームを開始します
-	go r.run()
-	// Webサーバーを起動します
-	if appengine.IsAppEngine() {
-		appengine.Main()
-	} else {
-		if err := http.ListenAndServe(":8080", nil); err != nil {
-			log.Fatal("ListenAndServe:", err)
-		}
-	}
-}
-
-func Index(w http.ResponseWriter, r *http.Request) {
+func Index(c echo.Context) error {
 	rooms, err := repository.NewRepository(DB).Fetch()
 	if err != nil {
 		panic(err)
@@ -94,10 +74,11 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	m := map[string]interface{}{
 		"rooms": rooms,
 	}
-	t := template.Must(template.ParseFiles("templates/index.html"))
-	if err := t.ExecuteTemplate(w, "index.html", m); err != nil {
-		log.Fatal(err)
-	}
+	// t := template.Must(template.ParseFiles("templates/index.html"))
+	// if err := t.ExecuteTemplate(w, "index.html", m); err != nil {
+	// 	log.Fatal(err)
+	// }
+	return c.Render(http.StatusOK, "index.html", m)
 }
 
 func NewRoom(w http.ResponseWriter, r *http.Request) {
@@ -122,4 +103,60 @@ func CreateRoom(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	http.Redirect(w, r, "/index", http.StatusFound)
+}
+
+var DB *gorm.DB
+
+func main() {
+	db, err := database.Connect()
+	if err != nil {
+		panic(err)
+	}
+	DB = db
+	defer db.Close()
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	// static file path
+	// http.HandleFunc("/vendor/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, r.URL.Path[1:])
+	// })
+	// http.HandleFunc("/css/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, r.URL.Path[1:])
+	// })
+	// http.HandleFunc("/avatars/", func(w http.ResponseWriter, r *http.Request) {
+	// 	http.ServeFile(w, r, r.URL.Path[1:])
+	// })
+
+	e := echo.New()
+	// setting static files
+	e.Static("/vendor", "vendor")
+	e.Static("/css", "css")
+	e.Static("/avatars", "avatars")
+
+	// setting tmeplates
+	t := &Template{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+	e.Renderer = t
+
+	e.GET("/index", Index)
+	// http.HandleFunc("/index", Index)
+	http.Handle("/chat/:id", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	// http.Handle("/room", roomArray[])
+	http.HandleFunc("/room/new", NewRoom)
+	http.HandleFunc("/room/create", CreateRoom)
+	// チャットルームを開始します
+	go r.run()
+	// Webサーバーを起動します
+	// if appengine.IsAppEngine() {
+	// 	appengine.Main()
+	// } else {
+	// 	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 		log.Fatal("ListenAndServe:", err)
+	// 	}
+	// }
+	// echoのサーバ起動
+	e.Logger.Fatal(e.Start(":8000"))
 }
