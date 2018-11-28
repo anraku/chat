b4a302e35e34f3cc6848551591c0b2991f29fbef
anraku <anraku90@gmail.com>
2018-11-29 06:22:20 +0900

    move Message struct to domain/

--- message_repository.go
+++ message_repository.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/anraku/chat/domain"
 	"github.com/jinzhu/gorm"
 )
 
@@ -14,27 +15,27 @@ func NewMessageRepository(db *gorm.DB) *MessageRepository {
 	}
 }
 
-func (r *MessageRepository) Fetch() (result []Message, err error) {
+func (r *MessageRepository) Fetch() (result []domain.Message, err error) {
 	err = r.DB.Debug().Table("messages").Find(&result).Error
 	return
 }
 
-func (r *MessageRepository) GetByID(id int) (result Message, err error) {
+func (r *MessageRepository) GetByID(id int) (result domain.Message, err error) {
 	err = r.DB.Debug().Table("messages").Where("id = ?", id).First(&result).Error
 	return
 }
 
-func (r *MessageRepository) GetByUserID(user_id int) (result Message, err error) {
+func (r *MessageRepository) GetByUserID(user_id int) (result domain.Message, err error) {
 	err = r.DB.Debug().Table("messages").Where("user_id = ?", user_id).First(&result).Error
 	return
 }
 
-func (r *MessageRepository) GetByRoomID(room_id int) (result []Message, err error) {
+func (r *MessageRepository) GetByRoomID(room_id int) (result []domain.Message, err error) {
 	err = r.DB.Debug().Table("messages").Where("room_id = ?", room_id).Find(&result).Error
 	return
 }
 
-func (r *MessageRepository) Create(message *Message) (err error) {
+func (r *MessageRepository) Create(message *domain.Message) (err error) {
 	err = r.DB.Debug().Table("messages").Create(message).Error
 	return
 }
